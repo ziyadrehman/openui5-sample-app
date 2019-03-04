8141991c1dfca016d31d7fa5c419d6164ed16ca9
Matthias Osswald <mat.osswald@sap.com>
2019-03-04 15:24:58 +0100

    Switch to karma-ui5

--- karma-ci.conf.js
+++ karma-ci.conf.js
@@ -1,14 +1,11 @@
-// Karma configuration
-// Generated on Wed Jun 13 2018 14:38:44 GMT+0200 (CEST)
-
 module.exports = function(config) {
+	"use strict";
+
 	require("./karma.conf")(config);
 	config.set({
 
-		// preprocess matching files before serving them to the browser
-		// available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
 		preprocessors: {
-			'{,!(test)}/*.js': ['coverage']
+			'{webapp,webapp/!(test)}/*.js': ['coverage']
 		},
 
 		coverageReporter: {
@@ -16,7 +13,7 @@ module.exports = function(config) {
 			reporters: [
 				{
 					type: 'html',
-					dir: '../coverage/'
+					dir: 'coverage'
 				},
 				{
 					type: 'text'
@@ -32,23 +29,10 @@ module.exports = function(config) {
 			}
 		},
 
-		client: {
-			qunit: {
-				showUI: false
-			}
-		},
-
-		// test results reporter to use
-		// possible values: 'dots', 'progress'
-		// available reporters: https://npmjs.org/browse/keyword/karma-reporter
 		reporters: ['progress', 'coverage'],
 
-		// start these browsers
-		// available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
 		browsers: ['ChromeHeadless'],
 
-		// Continuous Integration mode
-		// if true, Karma captures browsers, runs the tests and exits
 		singleRun: true
 
 	});
